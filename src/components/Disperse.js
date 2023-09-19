eaa8abb23561cc28e540a9236a36201f23ca613c
Amritesh verma <amriteshverma129@gmail.com>
2023-09-19 16:27:51 +0530

    Update the code

--- src/components/Disperse.js
+++ src/components/Disperse.js
@@ -1,77 +1,140 @@
 import React, { useState } from "react";
 
+function LineNumberTextArea({ value, onChange }) {
+  return (
+    <div className="line-number-textarea">
+      <div className="line-numbers">
+        {value.split("\n").map((_, index) => (
+          <div key={index} className="line-number">
+            {index + 1}
+          </div>
+        ))}
+      </div>
+      <div>
+        <textarea value={value} onChange={onChange} />
+      </div>
+    </div>
+  );
+}
+
 function Disperse() {
   const [value, setValue] = useState("");
-  const [keepFirstOneValue, setKeepFirstOneValue] = useState("");
-  const [combineBalancesValue, setConbineBalancesValue] = useState("");
-  const [errorMessage, setErrorMessage] = useState("");
+  const [errorMessage, setErrorMessage] = useState([]);
+  const [successMessage, setSuccessMessage] = useState("");
+  const [duplicate, setDuplicate] = useState(false);
+
+  const clearMessages = () => {
+    setErrorMessage([]);
+    setSuccessMessage("");
+    setDuplicate(false);
+  };
 
   const onSubmit = () => {
-    setErrorMessage("");
+    clearMessages();
     const arr = value.split("\n").filter((item) => item?.trim() !== "");
+
     if (arr.length === 0) {
-      setErrorMessage("Please enter the address and amount ");
+      setErrorMessage(["Please enter the address and amount"]);
       return;
     }
-    const newMap = new Map();
+
+    const addressMap = new Map();
 
     for (let i = 0; i < arr.length; i++) {
       const str = arr[i];
       const parts = str.split(/[=,\s]/);
+
       if (parts.length !== 2 || isNaN(parts[1])) {
-        setErrorMessage(`Line ${i + 1} wrong amount`);
+        setErrorMessage([`Line ${i + 1} has a wrong amount`]);
         return;
       }
-      if (newMap.has(parts[0])) {
-        newMap.set(parts[0], newMap.get(parts[0]) + 1);
+
+      if (addressMap.has(parts[0])) {
+        addressMap.get(parts[0]).push(i + 1);
       } else {
-        newMap.set(parts[0], 1);
+        addressMap.set(parts[0], [i + 1]);
       }
     }
-    keepFirstOne(arr);
-    combineBalances(arr);
+    const a = [];
+    addressMap.forEach((lineNumbers, address) => {
+      console.log(errorMessage);
+      if (lineNumbers.length > 1) {
+        a.push(
+          `Address ${address} has duplicates in lines: ${lineNumbers.join(",")}`
+        );
+      }
+    });
+
+    if (a.length) {
+      setErrorMessage(a);
+      setDuplicate(true);
+    } else {
+      setSuccessMessage("Successful");
+    }
   };
 
-  const keepFirstOne = (arr) => {
-    const newMap = new Map();
+  const keepFirstOne = () => {
+    clearMessages();
+    const arr = value.split("\n").filter((item) => item?.trim() !== "");
+    const addressMap = new Map();
+
     const newArr = arr.filter((str) => {
       const parts = str.split(/[=,\s]/);
-      if (newMap.has(parts[0])) {
-        return false;
-      } else {
-        newMap.set(parts[0], parts[1]);
+      if (!addressMap.has(parts[0])) {
+        addressMap.set(parts[0], true);
         return true;
       }
+      return false;
     });
-    setKeepFirstOneValue(newArr.join("\n"));
+
+    setValue(newArr.join("\n"));
   };
 
-  const combineBalances = (arr) => {
-    const newMap = new Map();
+  const combineBalances = () => {
+    clearMessages();
+    const arr = value.split("\n").filter((item) => item?.trim() !== "");
+    const addressMap = new Map();
+
     arr.forEach((str) => {
       const parts = str.split(/[=,\s]/);
-      if (newMap.has(parts[0])) {
-        newMap.set(
+      if (addressMap.has(parts[0])) {
+        addressMap.set(
           parts[0],
-          String(Number(newMap.get(parts[0])) + Number(parts[1]))
+          String(Number(addressMap.get(parts[0])) + Number(parts[1]))
         );
       } else {
-        newMap.set(parts[0], parts[1]);
+        addressMap.set(parts[0], parts[1]);
       }
     });
-    const combinedArray = [];
-    newMap.forEach((value, key) => {
-      combinedArray.push(key + " " + value);
-    });
-    setConbineBalancesValue(combinedArray.join("\n"));
+
+    const combinedArray = Array.from(addressMap.entries())
+      .map(([address, value]) => `${address} ${value}`)
+      .join("\n");
+
+    setValue(combinedArray);
   };
 
   return (
     <div>
-      <textarea value={value} onChange={(e) => setValue(e.target.value)} />
-      {errorMessage && (
+      <LineNumberTextArea
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+      {errorMessage && errorMessage.length && duplicate ? (
+        <div className="disperse-errorMessage-duplicacy">
+          <div>Duplicate Error</div>
+          <div>
+            <span onClick={() => keepFirstOne()}>Keep The First One</span>
+            <span></span>
+            <span onClick={() => combineBalances()}>Combine Balance</span>
+          </div>
+        </div>
+      ) : (
+        <></>
+      )}
+      {errorMessage && errorMessage.length ? (
         <div className="disperse-errorMessage">
-          <span>
+          <div>
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="30"
@@ -84,46 +147,36 @@ function Disperse() {
                 r="12"
                 fill="transparent"
                 stroke="red"
-                stroke-width="2"
+                strokeWidth="2"
               />
               <text
                 x="50%"
                 y="50%"
-                text-anchor="middle"
-                alignment-baseline="middle"
-                font-size="18"
-                font-weight="bold"
+                textAnchor="middle"
+                alignmentBaseline="middle"
+                fontSize="18"
+                fontWeight="bold"
                 fill="red"
               >
                 i
               </text>
             </svg>
-          </span>
-          <span>{errorMessage}</span>
+          </div>
+          <div>
+            {errorMessage.map((error, index) => (
+              <div key={index}>{error}</div>
+            ))}
+          </div>
         </div>
+      ) : (
+        <></>
+      )}
+      {successMessage && (
+        <div className="disperse-successMessage">{successMessage}</div>
       )}
       <button className="disperse-btn" onClick={() => onSubmit()}>
         Next
       </button>
-      {keepFirstOneValue.trim() ? (
-        <>
-          <div>keepFirstOneValue</div>
-          <div></div>
-          <textarea value={keepFirstOneValue} />
-        </>
-      ) : (
-        <></>
-      )}
-      {combineBalancesValue.trim() ? (
-        <>
-          <div>combineBalances</div>
-          <div>
-            <textarea value={combineBalancesValue} />
-          </div>
-        </>
-      ) : (
-        <></>
-      )}
     </div>
   );
 }
